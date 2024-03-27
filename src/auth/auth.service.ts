75790144b9bf353e3afe77135cf2d98ef8a5a547
fabrichgit <fabrichjean@gmail.com>
2024-03-27 20:25:27 +0200

    feat: creating file .ds via api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Res } from '@nestjs/common';
 import { CreateUserDTO } from 'src/DTO/create-user.dto';
 import { OtpSerive } from 'src/otp.service';
+import * as fs from 'node:fs'
 
 @Injectable()
 export class AuthService {
@@ -10,10 +11,30 @@ export class AuthService {
         return await this.otpService.sendOtp({mail})
     }
     
-    async register(req: CreateUserDTO){
-        console.log(req);
-        
-        const verfied = await this.otpService.verifyOtp({mail: req.mail, otp: req.otp})        
-        return verfied;
+    async register(user: CreateUserDTO){
+
+        const verfied = await this.otpService.verifyOtp({mail: user.mail, otp: user.otp})
+        if (verfied) {
+
+            const badgeContent = JSON.stringify(({...user, otp: undefined}));
+
+            fs.writeFile(`src/Badges/DontShare${user.id}.ds`, badgeContent, (err) => {
+                if (err) {
+                  console.error(err);
+                } else {
+                  console.log('Fichier .txt généré avec succès');
+                }
+              });
+
+            // Téléchargez le fichier de badge numérique en tant que réponse HTTP
+            // res.download(filePath, `DontShare_${user.id}.txt`, () => {
+            //   // Supprimez le fichier de badge après le téléchargement
+            //   fs.unlinkSync(filePath);
+            // });
+            
+            // return {
+            //     message: "Successfully !"
+            // }
+        }
     }
 }
